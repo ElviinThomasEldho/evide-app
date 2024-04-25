6b30508b35eaf9e881b371448a90e7ac50283b5f
kuru <kururox01@gmail.com>
2024-04-25 20:03:48 +0530

    Added MultiLanguage Support

--- evide-integrated/App.js
+++ evide-integrated/App.js
@@ -3,11 +3,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from './screens/Home.js';
 import RouteDetails from './screens/RouteDetails.js';
+import LanguageScreen from './components/LanguageScreen';
+import { LanguageProvider } from './context/LanguageContext';
+import { useTranslation } from 'react-i18next';
 
 const Drawer = createDrawerNavigator();
 
 export default function App(){
+  const { t } = useTranslation();
   return (
+    <LanguageProvider>
     <NavigationContainer>
       <Drawer.Navigator
         drawerStyle={{ backgroundColor: '#FFC75B' }} // Set background color for the drawer
@@ -16,8 +21,10 @@ export default function App(){
         }}
       >
         <Drawer.Screen name="RouteDetails" component={RouteDetails} />
-        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name={t('home')} component={Home} />
+        <Drawer.Screen name="Change Language" component={LanguageScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
+    </LanguageProvider>
   );
 };
